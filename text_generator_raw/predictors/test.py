8f3cb0477844f5c7b82a0a374cbdd52e7f181794
carlos9310 <52119171+carlos9310@users.noreply.github.com>
2019-08-29 21:57:53 +0800

    Update test.py

--- text_generator_raw/predictors/test.py
+++ text_generator_raw/predictors/test.py
@@ -9,7 +9,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--config_path", help="config path of model")
 args = parser.parse_args()
 
-with open(os.path.join(os.path.abspath(os.path.dirname(os.getcwd())), args.config_path), "r") as fr:
+# 当前目录的上一级目录
+with open(os.path.join(os.path.dirname(os.getcwd()), args.config_path), "r") as fr:
     config = json.load(fr)
 
 # with open("E:/jiangxinyang/pa_smart_city_nlp/text_generator/config/seq2seq_bigru_config.json", "r") as fr:
